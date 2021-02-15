14831868603581ca211e57c2dab66c0404b01c90
jolub <jolub@DESKTOP-TVE3LLQ>
2021-02-15 19:03:54 +0100

    Subida final

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.Bravo_Gonzalez_JoseLuis.calculator"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.Bravo_Gonzalez_JoseLuis.calculator","l":"Calculator"}];updateSearchResults();
